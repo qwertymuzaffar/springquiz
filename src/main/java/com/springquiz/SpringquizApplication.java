666e9bec7bfad55679bbe1e552c2144d95187a92
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-07-11 19:33:52 -0400

    Initial commit

---
+++ src/main/java/com/springquiz/SpringquizApplication.java
@@ -0,0 +1,13 @@
+package com.springquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringquizApplication.class, args);
+	}
+
+}
